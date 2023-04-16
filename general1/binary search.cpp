014fd99566e4317b003638e1c61ad83e25cf853f
vedansh0739 <93564155+vedansh0739@users.noreply.github.com>
2023-04-16 17:05:37 +0530

    Update binary search.cpp

--- general1/binary search.cpp
+++ general1/binary search.cpp
@@ -4,3 +4,16 @@ while (!ok(x+b)) x += b;
 }
 int k = x+1;
 
+
+
+
+//another example
+bool isPerfectSquare(int num) {
+        long long n=num;
+        long long x = -1;
+for (long long b = 100000; b >= 1; b /= 2) {
+while ((x+b)*(x+b)<=n) x += b;
+}
+long long k = x;
+if(k*k==num)return true;
+return false;
