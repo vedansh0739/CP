7606f30fece5ab284c5e4a6eee1005f0214b16c6
vedansh0739 <93564155+vedansh0739@users.noreply.github.com>
2023-04-18 21:28:03 +0530

    Update binary search.cpp

--- general1/binary search.cpp
+++ general1/binary search.cpp
@@ -1,40 +1,13 @@
-ll x = -1;
-for (ll b = z; b >= 1; b /= 2) {
-while (!ok(x+b)) x += b;
-}
-int k = x+1;
-
-
-
-
-//another example
-bool isPerfectSquare(int num) {
-        long long n=num;
-        long long x = -1;
-        for (long long b = 100000; b >= 1; b /= 2) {
-            while ((x+b)*(x+b)<=n) x += b;
-        }
-        long long k = x;
-        if(k*k==num)return true;
-        return false;
-}
-
-
-
-
-//find min possible
-while(low <= high)
+while(low<=high)
         {
-            int mid = low + (high - low) / 2;
-            
-            if(is_possible(weights, mid, days))
-            {
-                minians = mid;
-                
-                high = mid - 1;
-            }
+            mid=(low+high)/2;
+            if(ok(mid))
+                {
+                    ans=mid;
+                    low=mid+1;
+                }
             else
-            {
-                low = mid + 1;
-            }
+                high=mid-1;
         }
+
+
