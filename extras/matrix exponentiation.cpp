7a7b07ca41275e6212e486cf28ebbf148a66910a
vedansh0739 <93564155+vedansh0739@users.noreply.github.com>
2023-05-01 17:41:57 +0530

    Create matrix exponentiation.cpp

---
+++ extras/matrix exponentiation.cpp
@@ -0,0 +1,155 @@
+//https://www.youtube.com/watch?v=X-91JW7u_iw&list=PL5DyztRVgtRWblnyEHtajWijQffrfwLz6&index=13&ab_channel=CodeNCode
+//find fib in log time with the help of matrix exponentiation
+#include<bits/stdc++.h>
+using namespace std;
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define nline "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define debu printf("Running %s on line %d...\n",__FUNCTION__,__LINE__)
+#define rep(x,y,z) for(ll x=y;x<=z;x++)
+#define per(x,y,z) for(ll x=y;x>=z;x--)
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+//typedef tree<pair<int, int>, null_type, less<pair<int, int>>, rb_tree_tag, tree_order_statistics_node_update > pbds; // find_by_order, order_of_key
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+/*---------------------------------------------------------------------------------------------------------------------------*/
+ll gcd(ll a, ll b) {if (b > a) {return gcd(b, a);} if (b == 0) {return a;} return gcd(b, a % b);}
+ll expo(ll a, ll b, ll mod) {ll res = 1; while (b > 0) {if (b & 1)res = (res * a) % mod; a = (a * a) % mod; b = b >> 1;} return res;}
+void extendgcd(ll a, ll b, ll*v) {if (b == 0) {v[0] = 1; v[1] = 0; v[2] = a; return ;} extendgcd(b, a % b, v); ll x = v[1]; v[1] = v[0] - v[1] * (a / b); v[0] = x; return;} //pass an arry of size1 3
+ll mminv(ll a, ll b) {ll arr[3]; extendgcd(a, b, arr); return arr[0];} //for non prime b
+ll mminvprime(ll a, ll b) {return expo(a, b - 2, b);}
+bool revsort(ll a, ll b) {return a > b;}
+ll combination(ll n, ll r, ll m, ll *fact, ll *ifact) {ll val1 = fact[n]; ll val2 = ifact[n - r]; ll val3 = ifact[r]; return (((val1 * val2) % m) * val3) % m;}
+void google(int t) {cout << "Case #" << t << ": ";}
+vector<ll> sieve(int n) {int*arr = new int[n + 1](); vector<ll> vect; for (int i = 2; i <= n; i++)if (arr[i] == 0) {vect.push_back(i); for (int j = 2 * i; j <= n; j += i)arr[j] = 1;} return vect;}
+ll mod_add(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a + b) % m) + m) % m;}
+ll mod_mul(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a * b) % m) + m) % m;}
+ll mod_sub(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a - b) % m) + m) % m;}
+ll mod_div(ll a, ll b, ll m) {a = a % m; b = b % m; return (mod_mul(a, mminvprime(b, m), m) + m) % m;}  //only for prime m
+ll phin(ll n) {ll number = n; if (n % 2 == 0) {number /= 2; while (n % 2 == 0) n /= 2;} for (ll i = 3; i <= sqrt(n); i += 2) {if (n % i == 0) {while (n % i == 0)n /= i; number = (number / i * (i - 1));}} if (n > 1)number = (number / n * (n - 1)) ; return number;} //O(sqrt(N))
+/*--------------------------------------------------------------------------------------------------------------------------*/
+
+
+#define N 101 
+ll ar[101][101];
+ll i[101][101];
+
+ll m;
+void disp(ll B[][N],ll m){
+    rep(x,1,m){rep(y,1,m){cout<<B[x][y]<<" ";}cout<<nline;}
+}
+
+void mul(ll A[][N],ll B[][N],ll m){
+    ll res[m+1][m+1];
+    rep(i,1,m){
+        rep(j,1,m){
+            res[i][j]=0;
+            rep(k,1,m){
+                 res[i][j]=(res[i][j] % MOD + ((A[i][k] % MOD) * (B[k][j] % MOD) % MOD)) % MOD;
+            }
+        }
+    }
+    rep(x,1,m)rep(y,1,m)A[x][y]=res[x][y];
+}
+
+void mul1(ll ar[][1],ll ia[][N],ll m){
+    ll res[m+1][m+1];
+    rep(i,1,m){
+        rep(j,1,m){
+            res[i][j]=0;
+            rep(k,1,m){
+                 res[i][j]=(res[i][j] % MOD + ((ar[i][k] % MOD) * (ia[k][j] % MOD) % MOD)) % MOD;
+            }
+        }
+    }
+    rep(x,1,m)rep(y,1,m)ia[x][y]=res[x][y];
+}
+
+void solve(){ 
+ll a,b,n;
+cin>>a>>b>>n;
+if(n==0){cout<<a<<nline;return;}
+if(n==1){cout<<b<<nline;return;}
+n--;
+m=2;
+rep(x,1,m)rep(y,1,m){if(x==y)i[x][y]=1;else i[x][y]=0;}
+ar[1][1]=0;
+ar[2][1]=1;
+ar[1][2]=1;
+ar[2][2]=1;
+n++;
+while(n){
+    
+    if(n%2){
+        mul(i,ar,m);
+        n--;
+    }
+    else{
+        mul(ar,ar,m);
+        n=n/2;
+    }
+}
+
+
+
+cout<<((a*i[1][1]%MOD)+(b*i[2][1]%MOD))%MOD<<nline;
+
+
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("Error.txt", "w", stderr);
+#endif
+    fastio();
+    ll t=1;
+    cin>>t;
+    for(int i=1;i<=t;i++){
+        solve();
+    }
+
+
+
+
+    return 0;
+}
+
+
+
+
