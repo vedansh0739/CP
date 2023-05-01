c5d16b900b75803d9052c00cebf7d4abf04aa75d
vedansh0739 <93564155+vedansh0739@users.noreply.github.com>
2023-05-02 00:29:46 +0530

    Update sieve.cpp

--- general1/sieve.cpp
+++ general1/sieve.cpp
@@ -1,53 +1,31 @@
-
-        #include<bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-
-
-
-        const int TOT = 100000;
-//         map<int,int> ar[TOT+1]; 
-        void sieve1(){
-            int isPrime[TOT + 1];
-            for(int i = 0; i <=TOT; ++i) {
-                isPrime[i] = i%2;
-            }
-            isPrime[1] = false;
-            isPrime[2] = true;
-            for(int i = 3; i*i <=TOT ; i+=2) {               //no need to anki this, anki the non- optimized version's forgettable wali lines
-                    //(i.e., the lines which are not obvious when thinking about the algorithm in mind)
-                if(isPrime[i]){
-                    for(int j = i*i; j <=TOT ; j+=i) {
-                        isPrime[j] = false;
-                    }
-                }
-            }
-            vector<int> prime;
-            for(int i = 0; i <=TOT; ++i) {
-                if(isPrime[i]){
-                    prime.push_back(i);
-                }
-//             }
-//             for(int i = 2; i <=TOT; ++i) {
-//                 int x = i;
-//                 for(int j = 0; j <prime.size() && prime[j] * prime[j]<=x; ++j) {
-//                     while(x%prime[j]==0){
-//                         ar[i][prime[j]]++;
-//                         x/=prime[j];
-//                     }
-//                 }
-//                 if(x>1){
-//                     ar[i][x]++;
-//                 }
-            }
-        }
-
-
-
-        int main() {
-            ios_base::sync_with_stdio(false);
-            cin.tie(nullptr);
-            cout.tie(nullptr);
-            sieve1();
-        
-        }
-
+vector <bool> v(100000000,true);
+int pri[8000000];
+int main()
+{
+    long long int n = 100000000;
+    long int i,j ;
+    v[1] = false;
+    v[0] = false;
+    for(i=4;i<=n;i+=2)
+     v[i]=false;
+      for(i=3;i*i<=n;i+=2)
+     {
+           if(v[i])
+             {
+                for(j=i*i;j<=n;j=j+(2*i))
+                v[j] = false;
+             }
+     }
+       j=0;
+       pri[0] = 2;
+       for(i=3;i<=n;i+=2)
+      {
+        if(v[i])
+        pri[++j]=i;
+       }
+       for(i=1;i<=j;i+=100)
+      cout<<pri[i-1]<<"\n";
+      return 0;
+   }
